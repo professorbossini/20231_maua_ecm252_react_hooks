c61df19c8a3f4b4ff9e61349ade365567ae13bb9
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-20 12:51:57 -0300

    apostila08(hooks): fim da secao 2.5
    - prime-stuff instadas
    - componente Accordion criado
    - itens exibidos numa lista simples

---
+++ src/components/Accordion.js
@@ -0,0 +1,19 @@
+//rafce
+import React from 'react'
+
+const Accordion = ({itens}) => {
+  return (
+    <div>
+      {
+        itens.map((item, indice) => {
+          return <div key={indice}>
+              <h4>{item.titulo}</h4>
+              <p>{item.conteudo}</p>
+          </div>
+        })  
+      }
+    </div>
+  )
+}
+
+export default Accordion
