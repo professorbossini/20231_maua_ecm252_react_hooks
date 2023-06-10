09d5907c74981bd0f538cdc4e6580ecad275e228
Rodrigo Bossini <professorbossini@gmail.com>
2023-06-10 11:02:55 -0300

    apostila08(hooks): fim da secao 2.11
    - Accordion implementado usando useState
    - Começar fazendo a busca (ela fará uso de useEffect)

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,42 @@
 //rafce
-import React from 'react'
+import React, { useState } from 'react'
+import { Card } from 'primereact/card'
+import './Accordion.css'
 
 const Accordion = ({itens}) => {
-  return (
-    <div>
-      {
-        itens.map((item, indice) => {
-          return <div key={indice}>
-              <h4>{item.titulo}</h4>
-              <p>{item.conteudo}</p>
-          </div>
-        })  
-      }
-    </div>
-  )
-}
-
+  const [indiceAtivo, setIndiceAtivo] = useState(0)
+  const itemClicado = (indice) => {
+    // errado!indiceAtivo = indice
+    setIndiceAtivo(indice)
+  }
+  const expressaoJSX = itens.map((item, indice) => {
+    const classExibirConteudo = indice === indiceAtivo ? '' : 'hidden'
+    const classExibirIcone = indice === indiceAtivo ? 'pi-angle-down' : 'pi-angle-right'
+    return(
+      <Card id="accordion" key={indice} className='border-1 border-400'>
+        <div onClick={() => itemClicado(indice)}>
+          <i className={`pi ${classExibirIcone}`}></i>
+          <h5 className="inline ml-3">{item.titulo}</h5>
+        </div>
+        <p className={classExibirConteudo}>{item.conteudo}</p>
+      </Card>
+    )
+  })
+  
+  
+  return <div>
+    {expressaoJSX}
+  </div>
+  }
+  
+  // <div>
+  //   {
+  //     itens.map((item, indice) => {
+  //       return <div key={indice}>
+  //           <h4>{item.titulo}</h4>
+  //           <p>{item.conteudo}</p>
+  //       </div>
+  //     })  
+  //   }
+  // </div>
 export default Accordion
